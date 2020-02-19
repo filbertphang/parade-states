2fa7acd4b2f24d0e38d5bbbcc5415c650e3c0254
filbert <fillberth@gmail.com>
2020-02-19 13:04:39 +0800

    heroku setup attempt #1

--- parade_states/parade_states/wsgi.py
+++ parade_states/parade_states/wsgi.py
@@ -14,3 +14,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'parade_states.settings')
 
 application = get_wsgi_application()
+
+# from helloworld.wsgi import HelloWorldApplication
+# application = HelloWorldApplication(application)
---
+++ parade_states/parade_states_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ParadeStatesAppConfig(AppConfig):
+    name = 'parade_states_app'
